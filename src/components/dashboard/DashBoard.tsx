0d7c5d87d73a426936932f08b074f73ce92735cb
Prathamesh0009M <prathameshj776@gmail.com>
2025-06-25 00:28:39 +0530

    Fixed build issue after testing with npm run build

--- src/components/dashboard/DashBoard.tsx
+++ src/components/dashboard/DashBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import type { Session } from "mooterview-client";
 import { getAllSessionByUserId } from '../../utils/handlers/getAllSessionById';
 import { getTokenData } from '../../utils/constants';
--- src/components/sessionInfo/SessionOfProblem.tsx
+++ src/components/sessionInfo/SessionOfProblem.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { getSessionById } from '../../utils/handlers/getSessionById';
 import type { Session } from "mooterview-client";
