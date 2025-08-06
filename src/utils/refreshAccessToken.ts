23009d1f1752aef2a4f5fe6338c75e52f6e347e9
Prathamesh Jadhav <161243490+Prathamesh0009M@users.noreply.github.com>
2025-08-06 16:25:48 +0530

    SignUp RefreshToken & SignUp/login prompt solved  (#65)
    * SignUp RefreshToken issue solved
    
    * LogIn/SignUp Prompt resolved

--- src/utils/refreshAccessToken.ts
+++ src/utils/refreshAccessToken.ts
@@ -4,19 +4,26 @@ import { BASE_URL, getTokenData } from "./constants";
 export const refreshAccessToken = async () => {
   const tokens = getTokenData();
   if (!tokens?.refreshToken) {
+    window.location.href = "/log-in";
     throw new Error("No refresh token available.");
   }
 
-  const response = await axios.post(`${BASE_URL}/auth/refresh`, {
-    refreshToken: tokens.refreshToken,
-  });
+ try {
+    const response = await axios.post(`${BASE_URL}/auth/refresh`, {
+      refreshToken: tokens.refreshToken,
+    });
 
-  const updated = {
-    ...tokens,
-    accessToken: response.data.accessToken,
-    idToken: response.data.idToken,
-  };
+    const updated = {
+      ...tokens,
+      accessToken: response.data.AccessToken,
+      idToken: response.data.IdToken,
+    };
 
-  localStorage.setItem("userData", JSON.stringify(updated));
-  return updated.accessToken;
+    localStorage.setItem("userData", JSON.stringify(updated));
+    return updated.accessToken;
+  } catch (err) {
+
+    window.location.href = "/log-in";
+    throw new Error("Token refresh failed");
+  }
 };
